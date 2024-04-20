238a8fd0b8c4cb0ed9d7de197cdc6f3b810b37a1
jhosuapp <jhosuapvll@gmail.com>
2024-04-19 20:43:40 -0500

    feat: Types for events and listener callbacks

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+//Types
+import type { MouseEventHandler } from "react";
 //Components
 import { useState } from "react";
 import { RandomFox } from "./components/RandomFox";
@@ -8,17 +9,25 @@ const randomNumber = ():number => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
     //Custom type
-    type imageItem = { id:string, url:string };
+    type imageItem = { id:number, url:string };
     //State
-    const [images, setImages] = useState<Array<imageItem>>([
-        {"id": "1", "url": `https://randomfox.ca/images/${randomNumber()}.jpg`,},
-        {"id": "2", "url": `https://randomfox.ca/images/${randomNumber()}.jpg`,},
-        {"id": "3", "url": `https://randomfox.ca/images/${randomNumber()}.jpg`,},
-        {"id": "4", "url": `https://randomfox.ca/images/${randomNumber()}.jpg`,}
-    ]);
+    const [images, setImages] = useState<Array<imageItem>>([]);
+    const [counter, setCounter] = useState<number>(0);
+
+    const handleClick:MouseEventHandler<HTMLButtonElement> = (event):void=>{
+        //Prevent
+        event.preventDefault();
+        //Generate image
+        const generateImage = {"id": counter, "url": `https://randomfox.ca/images/${randomNumber()}.jpg`,};
+        //SetCounter
+        setCounter(counter + 1);
+        //SetImage
+        setImages([...images, generateImage ]);
+    }
 
     return (
         <>
+            <button onClick={ handleClick }>Add fox</button>
             {images.map(({ id, url })=>(
                 <div key={ id }>
                     {<RandomFox image={ url } alt={ 'Zorro' } />} 

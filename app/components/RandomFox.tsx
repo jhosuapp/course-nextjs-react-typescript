847318a642a3f7cd32e5fcd5b46bdeb743760c81
jhosuapp <jhosuapvll@gmail.com>
2024-04-21 19:02:06 -0500

    feat: render image in intersection

--- app/components/RandomFox.tsx
+++ app/components/RandomFox.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"; 
+import { useEffect, useRef, useState } from "react"; 
 interface Props {
     image: string;
     alt: string;
@@ -6,11 +6,31 @@ interface Props {
 
 //Type explicit
 const RandomFox = ( { image, alt } : Props ):JSX.Element =>{
-
+    //State
+    const [renderImage, setRenderImage] = useState<string>(
+        'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
+    );
+    //Node
     const node = useRef<HTMLImageElement>(null);
+    useEffect(()=>{
+        //Obsever
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    setRenderImage(image);
+                }
+            })
+        });
+
+        node.current && observer.observe(node.current);
+
+        return ()=>{
+            observer.disconnect();
+        }
+    }, [image]);
 
     return (
-        <img ref={ node } width={'100%'} src={ image } alt={ alt } />
+        <img ref={ node } className="bg-gray-300" width={'100%'} src={ renderImage } alt={ alt } />
     )
 }
 
